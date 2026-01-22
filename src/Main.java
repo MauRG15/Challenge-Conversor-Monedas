33b6d9eba4427208bb17e280417830367534a729
MauRG15 <rojasgallegosmauricio@gmail.com>
2026-01-22 14:29:27 -0600

    Modificar Main para ejecucion de la clase GsonManipulador

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,16 @@ import com.google.gson.JsonElement;
 public class Main {
     public static void main(String[] args){
         String respuestaSWAPI;
+        ConversorMoneda jsonAObjeto;
+
         String direccion = "https://v6.exchangerate-api.com/v6/09a76998f04a261837f2087d/pair/EUR/GBP/100";
         ApiComunicador apiComunicador = new ApiComunicador();
         respuestaSWAPI= apiComunicador.comunicarConAPI(direccion);
         System.out.println(respuestaSWAPI);
+
+        GsonManipulador gsonManipulador = new GsonManipulador();
+        jsonAObjeto = gsonManipulador.deserializarGSON(respuestaSWAPI);
+        System.out.println(jsonAObjeto);
+
     }
 }

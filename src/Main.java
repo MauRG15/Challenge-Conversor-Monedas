2b9f34e416eaba4e08e4b7e14c0a6cb26b3a82f1
MauRG15 <rojasgallegosmauricio@gmail.com>
2026-01-22 12:38:52 -0600

    Modificar clase Main para hacer uso de la clase ApiComunicador

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,10 @@ import com.google.gson.JsonElement;
 
 public class Main {
     public static void main(String[] args){
-        Gson gson = new Gson();
-
+        String respuestaSWAPI;
+        String direccion = "https://v6.exchangerate-api.com/v6/09a76998f04a261837f2087d/pair/EUR/GBP/100";
+        ApiComunicador apiComunicador = new ApiComunicador();
+        respuestaSWAPI= apiComunicador.comunicarConAPI(direccion);
+        System.out.println(respuestaSWAPI);
     }
 }

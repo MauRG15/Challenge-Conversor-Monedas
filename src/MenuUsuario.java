e3712f1f1990eea0113d6599e4847da052579837
MauRG15 <rojasgallegosmauricio@gmail.com>
2026-02-17 14:36:13 -0600

    Agregar metodo mostrarCurrencies para desplegar monedas disponibles

--- src/MenuUsuario.java
+++ src/MenuUsuario.java
@@ -1,5 +1,35 @@
+import java.util.List;
+
 public class MenuUsuario {
+    public void mostrarCurrencies(){
+        String respuestaAPI;
+        Currencies jsonAObjeto;
+        String apiKey = "09a76998f04a261837f2087d";
+
+        //Consumir API
+        String direccion = "https://v6.exchangerate-api.com/v6/"+apiKey+"/codes";
+        ApiComunicador apiComunicador = new ApiComunicador();
+        respuestaAPI= apiComunicador.comunicarConAPI(direccion);
+        System.out.println(respuestaAPI);
+
+        //Convertir Json a Objeto
+        GsonManipulador gsonManipulador = new GsonManipulador();
+        jsonAObjeto = gsonManipulador.deserializarGSON_Currencies(respuestaAPI);
+
+        for (List<String> c : jsonAObjeto.supported_codes()) {
+            String code = c.get(0);
+            String name = c.get(1);
+            System.out.println(code + " → " + name);
+        }
+    }
     public void mostrarMenu(){
-        System.out.println("***************\nBienvenido al conversor de monedas\n");
+        System.out.println("************************************************\n" +
+                "BIENVENIDO A LA APP DE CONVERSION DE MONEDAS\n\n" +
+                "Elija una de las siguientes opciones:\n" +
+                "1. Mostrar monedas disponibles.\n" +
+                "2. Realizar una conversión.\n\n" +
+                "9. Salir\n\n" +
+                "************************************************\n" +
+                "Opción a elegir: ");
     }
 }

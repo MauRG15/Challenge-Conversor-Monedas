1810cba5adf9f37e329a7e189fa039c071e9aa72
MauRG15 <rojasgallegosmauricio@gmail.com>
2026-02-17 14:37:52 -0600

    Inplementar loop para interaccion con el usuario

--- src/Main.java
+++ src/Main.java
@@ -2,34 +2,72 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        String respuestaSWAPI;
+        boolean ciclo = true;
+        String respuestaAPI;
         ConversorMoneda jsonAObjeto;
-        double cantidad;
+        double cantidad=0;
         String monedaActual;
         String monedaTarget;
         double resultadoConversion;
-
-        //Recibir datos de usuario
+        String eleccionUsuario;
+        String apiKey = "09a76998f04a261837f2087d";
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingresa la moneda base: ");
-        monedaActual = scanner.nextLine();
-        System.out.println("Ingresa la moneda objetivo: ");
-        monedaTarget = scanner.nextLine();
-        System.out.println("Ingresa la cantidad de dinero a convertir");
-        cantidad = scanner.nextDouble();
 
-        //Consumir API
-        String direccion = "https://v6.exchangerate-api.com/v6/09a76998f04a261837f2087d/pair/"+monedaActual+"/"+monedaTarget+"/"+cantidad;
-        ApiComunicador apiComunicador = new ApiComunicador();
-        respuestaSWAPI= apiComunicador.comunicarConAPI(direccion);
-        System.out.println(respuestaSWAPI);
+        //Mostrar Monedas
+        MenuUsuario menu = new MenuUsuario();
+        menu.mostrarCurrencies();
 
-        GsonManipulador gsonManipulador = new GsonManipulador();
-        jsonAObjeto = gsonManipulador.deserializarGSON(respuestaSWAPI);
-        System.out.println(jsonAObjeto);
+        while (ciclo) {
+            menu.mostrarMenu();
+            eleccionUsuario = scanner.nextLine();
+            switch (eleccionUsuario) {
+                case "1":
+                    menu.mostrarCurrencies();
+                    break;
+                case "2":
+                    //Recibir datos de usuario
+                    System.out.println("Ingresa el código de la moneda base (p.ej. EUR, MXN, etc): ");
+                    monedaActual = scanner.nextLine();
+                    System.out.println("Ingresa el código de la moneda objetivo: (p.ej. EUR, MXN, etc)");
+                    monedaTarget = scanner.nextLine();
+                    System.out.println("Ingresa la cantidad de dinero a convertir: ");
+                    if (scanner.hasNextDouble()){
+                        cantidad = scanner.nextDouble();
 
-        Calculadora calculadora = new Calculadora();
-        resultadoConversion = calculadora.conversion(jsonAObjeto,cantidad);
-        System.out.println(resultadoConversion);
+                    }
+                    else{
+                        System.out.println("Cantidad no valida, ingresa un numero decimal o entero.");
+                        scanner.nextLine();
+                        break;
+                    }
+                    scanner.nextLine();
+                    //Consumir API
+                    String direccion = "https://v6.exchangerate-api.com/v6/"+apiKey+"/pair/" + monedaActual + "/" + monedaTarget;
+                    ApiComunicador apiComunicador = new ApiComunicador();
+                    respuestaAPI = apiComunicador.comunicarConAPI(direccion);
+                    //System.out.println(respuestaAPI);//Verificar conexion con API
+
+                    //Convertir Json a Objeto
+                    GsonManipulador gsonManipulador = new GsonManipulador();
+                    jsonAObjeto = gsonManipulador.deserializarGSON(respuestaAPI);
+                    if (jsonAObjeto.result().equals("success")) {
+                        //System.out.println(jsonAObjeto);//Estado del objeto JSON
+                        Calculadora calculadora = new Calculadora();
+                        resultadoConversion = calculadora.conversion(jsonAObjeto, cantidad);
+                        System.out.printf("$%,.3f%n",resultadoConversion);
+                    }
+                    else{
+                        System.out.println("No se pudo encontrar el codigo de alguna de las monedas, por favor verifica que las escribiste correctamente e intentalo de nuevo.");
+                    }
+                    break;
+                case "9":
+                    System.out.println("Nos vemos, gracias por usar mi conversor :)");
+                    ciclo = false;
+                    break;
+                default:
+                    System.out.println("Elije una opción valida del menú, por favor.");
+                    break;
+            }
+        }
     }
 }

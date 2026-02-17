61e92b70d74ac3fff4615d09fec13ea937fd17e5
MauRG15 <rojasgallegosmauricio@gmail.com>
2026-02-17 15:23:20 -0600

    Cambiar el formato del resultado a 2 decimales

--- src/MenuUsuario.java
+++ src/MenuUsuario.java
@@ -10,12 +10,13 @@ public class MenuUsuario {
         String direccion = "https://v6.exchangerate-api.com/v6/"+apiKey+"/codes";
         ApiComunicador apiComunicador = new ApiComunicador();
         respuestaAPI= apiComunicador.comunicarConAPI(direccion);
-        System.out.println(respuestaAPI);
+        //System.out.println(respuestaAPI);//Estado de respuesta de API
 
         //Convertir Json a Objeto
         GsonManipulador gsonManipulador = new GsonManipulador();
         jsonAObjeto = gsonManipulador.deserializarGSON_Currencies(respuestaAPI);
 
+        System.out.println("MONEDAS DISPONIBLES PARA CONVERSION.\n");
         for (List<String> c : jsonAObjeto.supported_codes()) {
             String code = c.get(0);
             String name = c.get(1);
